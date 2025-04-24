fb0030e1b09c96ea21d07c1b9c90e35b0c77a102
JHyeok5 <jh0580jh@naver.com>
2025-04-24 18:31:34 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { TextContextProvider } from './context/TextContext';
@@ -8,10 +8,10 @@ import { TextContextProvider } from './context/TextContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <TextContextProvider>
         <App />
       </TextContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 ); 
