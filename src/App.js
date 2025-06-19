b8da2365ff7563e6a7eedb8fc0d78ae026ab8bbd
User <jh0580jh@naver.com>
2025-06-19 21:24:24 +0900

    fix: Update TextProvider to TextContextProvider and fix asset paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
 import { AppProvider } from './contexts/AppContext';
 import { AnalyticsProvider } from './contexts/AnalyticsContext';
+import { TextContextProvider } from './contexts/TextContext';
 import Layout from './components/layout/Layout';
 import DashboardPage from './pages/DashboardPage';
 import EditorPage from './pages/EditorPage';
@@ -12,28 +13,27 @@ import CoachingPage from './pages/CoachingPage';
 import ProfilePage from './pages/ProfilePage';
 import SubscriptionPage from './pages/SubscriptionPage';
 import UsageLimitModal from './components/common/UsageLimitModal';
-import { useTextContext } from './contexts/TextContext';
 
 function App() {
-  const { error } = useTextContext();
-
   return (
     <UserProvider>
       <AppProvider>
         <AnalyticsProvider>
-          <Layout>
-            <Routes>
-              <Route path="/" element={<DashboardPage />} />
-              <Route path="/editor" element={<EditorPage />} />
-              <Route path="/analysis" element={<AnalysisPage />} />
-              <Route path="/templates" element={<TemplatesPage />} />
-              <Route path="/coaching" element={<CoachingPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/subscription" element={<SubscriptionPage />} />
-              <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-            {error && error.includes('일일 사용량 한도') && <UsageLimitModal />}
-          </Layout>
+          <TextContextProvider>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<DashboardPage />} />
+                <Route path="/editor" element={<EditorPage />} />
+                <Route path="/analysis" element={<AnalysisPage />} />
+                <Route path="/templates" element={<TemplatesPage />} />
+                <Route path="/coaching" element={<CoachingPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/subscription" element={<SubscriptionPage />} />
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+              <UsageLimitModal />
+            </Layout>
+          </TextContextProvider>
         </AnalyticsProvider>
       </AppProvider>
     </UserProvider>
