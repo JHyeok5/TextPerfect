4a7d177886d05dad35a6c82f3187a6775bbeb750
User <jh0580jh@naver.com>
2025-06-19 23:48:16 +0900

    fix: useUser 훅에 컨텍스트 유효성 검사 추가

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -47,5 +47,9 @@ export function UserProvider({ children }) {
 }
 
 export function useUser() {
-  return useContext(UserContext);
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider');
+  }
+  return context;
 } 
