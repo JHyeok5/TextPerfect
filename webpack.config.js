a112c3f14d5bd0169b9719b401c89619ecef4dad
User <jh0580jh@naver.com>
2025-06-19 20:55:47 +0900

    fix: Update webpack config for React app and GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@
 const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://textperfect.space/";
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
@@ -18,17 +19,28 @@ module.exports = async (env, options) => {
     devtool: "source-map",
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
+      app: "./src/index.js",
       taskpane: ["./src/taskpane/taskpane.ts", "./src/taskpane/taskpane.html"],
       commands: "./src/commands/commands.ts",
     },
     output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
       clean: true,
+      publicPath: '/',
     },
     resolve: {
-      extensions: [".ts", ".html", ".js"],
+      extensions: [".ts", ".tsx", ".html", ".js", ".jsx"],
     },
     module: {
       rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          },
+        },
         {
           test: /\.ts$/,
           exclude: /node_modules/,
@@ -37,12 +49,16 @@ module.exports = async (env, options) => {
           },
         },
         {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+        {
           test: /\.html$/,
           exclude: /node_modules/,
           use: "html-loader",
         },
         {
-          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
           type: "asset/resource",
           generator: {
             filename: "assets/[name][ext][query]",
@@ -52,12 +68,30 @@ module.exports = async (env, options) => {
     },
     plugins: [
       new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+        chunks: ["polyfill", "app"],
+        favicon: "./public/favicon.ico",
+      }),
+      new HtmlWebpackPlugin({
         filename: "taskpane.html",
         template: "./src/taskpane/taskpane.html",
         chunks: ["polyfill", "taskpane"],
       }),
+      new HtmlWebpackPlugin({
+        filename: "commands.html",
+        template: "./src/commands/commands.html",
+        chunks: ["polyfill", "commands"],
+      }),
       new CopyWebpackPlugin({
         patterns: [
+          {
+            from: "public",
+            to: ".",
+            globOptions: {
+              ignore: ["**/index.html", "**/favicon.ico"],
+            },
+          },
           {
             from: "assets/*",
             to: "assets/[name][ext][query]",
@@ -75,11 +109,6 @@ module.exports = async (env, options) => {
           },
         ],
       }),
-      new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["polyfill", "commands"],
-      }),
     ],
     devServer: {
       headers: {
@@ -90,6 +119,7 @@ module.exports = async (env, options) => {
         options: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
       },
       port: process.env.npm_package_config_dev_server_port || 3000,
+      historyApiFallback: true,
     },
   };
 
