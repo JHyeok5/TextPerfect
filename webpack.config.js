ead41f4d3b8dbc859cf1284db4a9090fc56d437f
User <jh0580jh@naver.com>
2025-06-19 22:59:27 +0900

    chore: GitHub Pages 배포 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     polyfill: ['core-js/stable', 'regenerator-runtime/runtime']
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     publicPath: '/',
     clean: true
