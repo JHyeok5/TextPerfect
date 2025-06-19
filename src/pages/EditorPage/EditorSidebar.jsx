7f270e48f716cfe8d82554d80858fc6ea50b44b5
User <jh0580jh@naver.com>
2025-06-20 01:57:13 +0900

    Update EditorSidebar.jsx

--- src/pages/EditorPage/EditorSidebar.jsx
+++ src/pages/EditorPage/EditorSidebar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import SettingsPanel from '../../../components/editor/SettingsPanel';
+import SettingsPanel from '../../components/editor/SettingsPanel';
 
 export default function EditorSidebar() {
   // 임시 상태. 추후에 Context API나 다른 상태 관리 로직과 연결해야 합니다.
