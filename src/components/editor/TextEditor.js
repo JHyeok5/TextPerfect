56e43e97f665d786e95c9ced6e3ae166917c2e55
User <jh0580jh@naver.com>
2025-06-21 00:27:36 +0900

    Update TextEditor.js

--- src/components/editor/TextEditor.js
+++ src/components/editor/TextEditor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TextEditor = ({ value, onChange }) => {
   return (
@@ -14,6 +15,11 @@ const TextEditor = ({ value, onChange }) => {
       </div>
     </div>
   );
+};
+
+TextEditor.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default TextEditor; 
